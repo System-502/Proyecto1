9844a0ca30972f24ec86b1740be5e3975ba7440a
Carlos Olivares <usac.ingenieria.sistemas@gmail.com>
2023-09-13 06:02:14 +0000

    Prueba No.1

---
+++ Inteceptor.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+
+Class Prueba{
+
+
+	function ocultarDigito(){
+
+		// $controller = $observer->getControllerAction();
+// $this->redirect->redirect($controller->getResponse(), 'customer/account/login');
+// return $this;
+	
+	}
+
+
+
+}
